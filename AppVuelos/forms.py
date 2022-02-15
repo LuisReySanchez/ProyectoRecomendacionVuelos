0a67a3b65b9bd731b553e4cb70d47c1fab8c6d53
Luis Rey Sanchez <luen_2@yahoo.es>
2022-02-15 15:40:29 -0500

    15-02

--- AppVuelos/forms.py
+++ AppVuelos/forms.py
@@ -7,7 +7,7 @@ from django import forms
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from sqlalchemy import false
+# from sqlalchemy import false
 
 class OperadorFormulario(forms.Form):
 
